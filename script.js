fd03077f84c64246b3f08507845122ce7ac18086
Suy4Dever <khatbde180162@fpt.edu.vn>
2023-02-16 17:43:04 +0700

    add show panel when click button

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
 let imgList = ["/img/band-img.jpg", "/img/chicago.jpg", "/img/ny.jpg"];
-let locationList = ["Los Angeles","New York", "Chicago"];
-let descriptionList = ["We had the best time playing at Venice Beach!", "The atmosphere in New York is lorem ipsum.", "Thank you, Chicago - A night we won't forget."]
+let locationList = ["Los Angeles", "New York", "Chicago"];
+let descriptionList = [
+  "We had the best time playing at Venice Beach!",
+  "The atmosphere in New York is lorem ipsum.",
+  "Thank you, Chicago - A night we won't forget.",
+];
 
 let index = 0;
 setInterval(() => {
-    if(index === imgList.length) index = 0;
-    document.getElementsByClassName("band-img")[0].src = "http://127.0.0.1:5500" + imgList[index];
-    document.getElementsByClassName("performance-location")[0].textContent = locationList[index];
-    document.getElementsByClassName("performance-description")[0].textContent = descriptionList[index];
-    index++;
-}, 3000)
+  if (index === imgList.length) index = 0;
+  document.getElementsByClassName("band-img")[0].src =
+    "http://127.0.0.1:5500" + imgList[index];
+  document.getElementsByClassName("performance-location")[0].textContent =
+    locationList[index];
+  document.getElementsByClassName("performance-description")[0].textContent =
+    descriptionList[index];
+  index++;
+}, 3000);
+
+for (let i = 0; i < 3; i++) {
+  document
+    .getElementsByClassName("buy-ticket")
+    [i].addEventListener("click", () => {
+      document.getElementsByClassName(
+        "ticket-panel-container"
+      )[0].style.display = "block";
+    });
+}
