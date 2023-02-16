654cae35a1f0ab208367cfa646f421f4f2a59d47
Suy4Dever <khatbde180162@fpt.edu.vn>
2023-02-16 18:12:57 +0700

    fix the img

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let imgList = ["/img/band-img.jpg", "/img/chicago.jpg", "/img/ny.jpg"];
+let imgList = ["./img/band-img.jpg", "./img/chicago.jpg", "./img/ny.jpg"];
 let locationList = ["Los Angeles", "New York", "Chicago"];
 let descriptionList = [
   "We had the best time playing at Venice Beach!",
@@ -9,8 +9,7 @@ let descriptionList = [
 let index = 0;
 setInterval(() => {
   if (index === imgList.length) index = 0;
-  document.getElementsByClassName("band-img")[0].src =
-    "http://127.0.0.1:5500" + imgList[index];
+  document.getElementsByClassName("band-img")[0].src = imgList[index];
   document.getElementsByClassName("performance-location")[0].textContent =
     locationList[index];
   document.getElementsByClassName("performance-description")[0].textContent =
